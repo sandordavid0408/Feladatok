e4a2abe1b56d3cd5a374f6d6d0d07606ccee2472
Sandor David <sandor.david@student.nye.hu>
2026-02-16 10:10:51 +0100

    2. feladat

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double masodperc;
+        do {
+            System.out.print("Villamlas utan hany masodperccel hallottad a dorgest? ");
+            masodperc = scanner.nextDouble();
+        } while (masodperc < 0);
+
+        double tavolsagMeter = masodperc * 300;
+        System.out.println("A villamlas tavolsaga: " + tavolsagMeter + " meter");
+    }
+}
